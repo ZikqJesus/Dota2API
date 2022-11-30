cf2e04ae4be1807680696e9420fbeaed5da09067
ZikQJesus <99221885+ZikqJesus@users.noreply.github.com>
2022-11-30 07:06:31 +0300

    Delete Program.cs

--- DotaAPI/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using DotaAPI;
-
-DotaClient cl = new("Login", "password@", true);
-
-cl.Connect();
-cl.CreateLobby("huesosi", gamemode: DotaAPI.enums.Gamemodes.CM);
-cl.JoinTeam(DotaAPI.enums.DotaTeam.Pool, slot: 1);
